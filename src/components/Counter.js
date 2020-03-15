eea947f5954380c756cb854e29523ff60b58ce5d
MinG822 <aestas822@gmail.com>
2020-03-16 00:47:07 +0900

    multiple counters

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,21 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Counter.css'
 
-const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
   return (
     <div
       className="Counter"
-      onClick={onIncrement}
+      // 무한 렌더링을 피하기 위해 인자가 들어가면 함수 리턴값으로
+      onClick={() => onIncrement(index)}
       // 우클릭시 메뉴가 열리는 이벤트
       onContextMenu={
         (e) => {
           // 메뉴가 열리지 않음
           e.preventDefault()
-          onDecrement()
+          onDecrement(index)
         }
       }
       // onClick 이벤트와 겹치지 않으려면..?
-      onDoubleClick={onSetColor}
+      onDoubleClick={() => onSetColor(index)}
       style={{backgroundColor: color}}>
         {number}
       </div>
@@ -25,6 +26,7 @@ const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
 
 //propTypes 와 PropTypes 헷갈리지 않기
 Counter.propTypes = {
+  index: PropTypes.number,
   number: PropTypes.number,
   color: PropTypes.string,
   onIncrement: PropTypes.func,
@@ -33,6 +35,7 @@ Counter.propTypes = {
 }
 
 Counter.defaultProps = {
+  index: 0,
   number: 0,
   color: 'black',
   onIncrement: () => console.warn('onIncrement is not defined'),
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
-import CounterContainer from '../containers/CounterContainer'
+import Buttons from '../components/Buttons'
+import CounterContainer from '../containers/CounterListContainer'
+
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+
+import { getRandomColor } from '../utils'
 
 class App extends Component {
   render () {
+    //props를 어디서 받아옴?? index.js에서 넘겨주는 prop이없는데?
+    // App에서 onCreate, onRemove 액션을 발생시켰기 때문에 props로 가지고 있을 수 있는데,
+    // mapToDisPatch 부분을 주석처리한뒤 this.props를 로그찍으면 props가 존재하지 않음을 알 수 있다.
+    const { onCreate, onRemove } = this.props
     return (
-      <div>
+      <div className="App">
+        <Buttons
+          onCreate={onCreate}
+          onRemove={onRemove}
+        />
         <CounterContainer/>
       </div>
     )
   }
 }
 
-export default App
+const mapToDispatch = (dispatch) => ({
+  onCreate: () => dispatch(actions.create(getRandomColor())),
+  onRemove: (index) => dispatch(actions.remove(index)) 
+})
+
+// 파일 하나에서 컴포넌트를 정의하고 바로 연결해 주었다.
+// store에서 필요한 값이 없어서 mapStateToProps는 null로 설정
+export default connect(null, mapToDispatch)(App)
